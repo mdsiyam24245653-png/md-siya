74aefabce88843695fa8b0a6c59b3acf5bd92f94
mdsiyam24245653-png <mdsiyam24245653@gmail.com>
2026-04-04 02:28:52 +0600

    Update info.js

--- Script/commands/info.js
+++ Script/commands/info.js
@@ -7,7 +7,7 @@ module.exports.config = {
  name: "info",
  version: "1.0.0",
  hasPermssion: 0,
- credits: "🔰𝐑𝐀𝐇𝐀𝐓 𝐈𝐒𝐋𝐀𝐌🔰",
+ credits: "🔰𝐑𝐀𝐇𝐀𝐓 𝐈𝐒𝐋𝐀𝐌",
  description: "Show  Info",
  commandCategory: "info",
  usages: "info",
